f271ec33bd89446404b3d7c94287f6d3004269cf
Palloma Gualter <pallomagualter@gmail.com>
2020-11-19 16:42:16 -0300

    login

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createSwitchNavigator } from 'react-navigation';
 
 export default function App() {
   return (
@@ -10,6 +11,12 @@ export default function App() {
     </View>
   );
 }
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoginScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+})
 
 const styles = StyleSheet.create({
   container: {
