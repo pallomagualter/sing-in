39ef6fd736c7f9019fad7a43d5526eebdde77efc
Palloma Gualter <pallomagualter@gmail.com>
2020-11-23 15:12:37 -0300

    app

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
 }
 
 const AppSwitchNavigator = createSwitchNavigator({
-  LoadingScreen: LoginScreen,
+  LoadingScreen: LoadingScreen,
   LoginScreen: LoginScreen,
   DashboardScreen: DashboardScreen
 })
