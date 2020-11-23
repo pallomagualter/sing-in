86643f22ef2dc0bb050abf84c02eefcf1e68c33c
Palloma Gualter <pallomagualter@gmail.com>
2020-11-23 16:05:25 -0300

    firebase

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import DashboardScreen from './src/screens/DashboardScreen';
 
 import * as firebase from 'firebase';
 import { firebaseConfig } from './src/config';
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
