f7a60c8fa7abdf80dabbb2cc02cbdf6b5b640565
Palloma Gualter <pallomagualter@gmail.com>
2020-11-23 15:39:46 -0300

    app

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createSwitchNavigator } from 'react-navigation';
 
+import LoadingScreen from './src/screens/LoadingScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import DashboardScreen from './src/screens/DashboardScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
