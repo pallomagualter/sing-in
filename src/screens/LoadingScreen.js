bd0bb76453ad0240d9c658fa5ac60d697f6a1179
Palloma Gualter <pallomagualter@gmail.com>
2020-11-24 08:59:20 -0300

    loading

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 
 class LoadingScreen extends Component {
+  checkIfLoggedIn = () => {
+    firebase.auth().onAuthStateChanged(function(user)
+    {
+      if(user) {
+        this.props.navigation.navigate('DashboardScreen');
+      } else {
+        this.props.navigation.navigate('LoginScreen');
+      }
+    })
+  }
+
     render() {
         return (
             <View style={styles.container}>
